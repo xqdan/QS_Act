80fc485870f1b40ec97b4f55e43dce5c673a554a
Yusheng.Ma <Yusheng.Ma@streamcomputing.com>
2021-07-01 18:59:37 -0700

    fix gitignore

---
+++ cmake/depends/glog.cmake
@@ -0,0 +1,12 @@
+include(ExternalProject)
+
+ExternalProject_Add(libglog
+    URL https://github.com/google/glog/archive/refs/tags/v0.5.0.tar.gz
+    URL_HASH SHA256=eede71f28371bf39aa69b45de23b329d37214016e2055269b3b5e7cfd40b59f5
+    PREFIX glog
+    BUILD_IN_SOURCE 1
+    CMAKE_ARGS -S . -B build -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${DEPENDS_INSTALL_DIR}
+        -DWITH_GFLAGS=OFF -DWITH_GTEST=OFF -DWITH_UNWIND=OFF
+    BUILD_COMMAND cmake --build build --target glog
+    INSTALL_COMMAND cmake --build build --target install
+    )
---
+++ cmake/depends/http_parser.cmake
@@ -0,0 +1,12 @@
+include(ExternalProject)
+
+ExternalProject_Add(libhttp_parser
+    URL https://github.com/nodejs/http-parser/archive/4f15b7d510dc7c6361a26a7c6d2f7c3a17f8d878.tar.gz
+    URL_MD5 e6cb2c467d3916e6a3304c165f6a6026
+    PREFIX http_parser
+    CONFIGURE_COMMAND ""
+    BUILD_IN_SOURCE 1
+    BUILD_COMMAND make library
+    INSTALL_COMMAND make install PREFIX=${DEPENDS_INSTALL_DIR}
+    )
+
